4c3056849bb80672bec5b02da9d50ca2bd7ba723
furby™ <devs@wabi.foundation>
2023-08-20 10:11:48 -0500

    Support building Yams with visionOS.

--- Package.swift
+++ Package.swift
@@ -1,31 +1,38 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "Yams",
-    products: [
-        .library(name: "Yams", targets: ["Yams"])
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "CYaml",
-            exclude: ["CMakeLists.txt"],
-            cSettings: [.define("YAML_DECLARE_STATIC")]
-        ),
-        .target(
-            name: "Yams",
-            dependencies: ["CYaml"],
-            exclude: ["CMakeLists.txt"],
-            cSettings: [.define("YAML_DECLARE_STATIC")]
-        ),
-        .testTarget(
-            name: "YamsTests",
-            dependencies: ["Yams"],
-            exclude: ["CMakeLists.txt"],
-            resources: [
-                .copy("Fixtures/SourceKitten#289/debug.yaml"),
-            ]
-        )
-    ]
+  name: "Yams",
+  platforms: [
+    .macOS(.v14),
+    .visionOS(.v1),
+    .iOS(.v17),
+    .tvOS(.v16),
+    .watchOS(.v9),
+  ],
+  products: [
+    .library(name: "Yams", targets: ["Yams"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "CYaml",
+      exclude: ["CMakeLists.txt"],
+      cSettings: [.define("YAML_DECLARE_STATIC")]
+    ),
+    .target(
+      name: "Yams",
+      dependencies: ["CYaml"],
+      exclude: ["CMakeLists.txt"],
+      cSettings: [.define("YAML_DECLARE_STATIC")]
+    ),
+    .testTarget(
+      name: "YamsTests",
+      dependencies: ["Yams"],
+      exclude: ["CMakeLists.txt"],
+      resources: [
+        .copy("Fixtures/SourceKitten#289/debug.yaml"),
+      ]
+    ),
+  ]
 )
